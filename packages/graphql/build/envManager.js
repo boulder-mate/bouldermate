626cdd26b523e04f6bf60ee00842df1090151f70
Ethan Husband <ethan.husband@evup.com.au>
2023-06-15 18:30:43 +1000

    gitignore build

--- packages/graphql/build/envManager.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.REQUEST_PREFIX = void 0;
-const NODE_ENV = process.env.NODE_ENV;
-exports.REQUEST_PREFIX = NODE_ENV === "local" ? "http" : "https";
-if (!process.env.NODE_ENV) {
-    throw "Invalid Node env supplied!";
-}
-//# sourceMappingURL=envManager.js.map
