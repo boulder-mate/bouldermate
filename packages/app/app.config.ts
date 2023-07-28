b9b85268e583dcc9d985f627643420ef5473f571
Ethan Husband <ethan.husband@evup.com.au>
2023-07-28 23:42:18 +1000

    Park changes

--- packages/app/app.config.ts
+++ packages/app/app.config.ts
@@ -1,11 +1,11 @@
 import "dotenv/config";
-import { ExpoConfig, ConfigContext } from 'expo/config';
 
-export default ({ config }: ConfigContext): ExpoConfig => ({
-  ...config,
+export default {
+  expo: {
     owner: "ethanhusband",
     name: "BoulderMate",
     slug: "bouldermate",
+    scheme: "bouldermate",
     currentFullName: "@user/bouldermate",
     originalFullName: "@user/bouldermate",
     version: "1.0.0",
@@ -34,10 +34,22 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     android: {
       adaptiveIcon: {
         foregroundImage: "./assets/images/logo-whitebg.png",
-        backgroundColor: "#ffffff"
+        backgroundColor: "#ffffff",
       },
       package: "com.bouldermate",
-      versionCode: 100
+      versionCode: 100,
+    },
+    ios: {
+      bundleIdentifier: "com.bouldermate",
+      buildNumber: "1.0.0",
+      infoPlist: {
+        NSCameraUsageDescription:
+          "This app uses the camera to record climbs and upload routes.",
+        NSPhotoLibraryUsageDescription:
+          "This app uses the photo library to upload climbs and route images.",
+      },
+      usesAppleSignIn: true,
+
     },
     plugins: [],
     extra: {
@@ -50,4 +62,5 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
         projectId: process.env.EAS_PROJECT_ID,
       },
     },
-});
+  },
+};
