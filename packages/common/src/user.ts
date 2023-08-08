505d8e39be7d3a9f7784729a4c55f312d3a98731
Ethan Husband <ethan.husband@evup.com.au>
2023-08-08 22:29:53 +1000

    Update user to just have name, like Instagram

--- packages/common/src/user.ts
+++ packages/common/src/user.ts
@@ -1,36 +1,35 @@
-import { ID, LinkedID } from "./abstract"
+import { ID, LinkedID } from "./abstract";
 
 export type User = ID & {
-    username: string,
-    first_name: string,
-    last_name: string,
-    company_name: string, // Use this for Gym accounts instead of first + last
-    bio?: string,
-    email?: string, // Only excluded for Anon accounts!
-    phone_number?: string, // Optional - ideally included for Gyms
-    preferences: UserPreferences,
-    locations: LinkedID[],
-    routes: LinkedID[],
-    ratings: LinkedID[],
-    comments: LinkedID[],
-    verified?: boolean,
-    anonymous?: boolean,
-    
-    // Routesetters only
-    routes_created?: LinkedID[],
-    company?: LinkedID[],
-    years_experience?: number,
+  username: string;
+  name: string;
+  company_name: string; // Use this for Gym accounts instead of first + last
+  bio?: string;
+  email?: string; // Only excluded for Anon accounts!
+  phone_number?: string; // Optional - ideally included for Gyms
+  preferences: UserPreferences;
+  locations: LinkedID[];
+  routes: LinkedID[];
+  ratings: LinkedID[];
+  comments: LinkedID[];
+  verified?: boolean;
+  anonymous?: boolean;
 
-    // Gym admin only
-    location_admin?: LinkedID[] // Gym the 
-    
-    // Activity for a user will be stored by their user ID in a different collection
-}
+  // Routesetters only
+  routes_created?: LinkedID[];
+  company?: LinkedID[];
+  years_experience?: number;
+
+  // Gym admin only
+  location_admin?: LinkedID[]; // Gym the
+
+  // Activity for a user will be stored by their user ID in a different collection
+};
 
 export type UserPreferences = {
-    // These will get fleshed out with the frontend
-    private?: boolean
-}
+  // These will get fleshed out with the frontend
+  private?: boolean;
+};
 
 // Activity related features will come later
 
@@ -46,5 +45,5 @@ export type UserPreferences = {
 // export enum ActivityPath {
 //     Project = "Route",
 //     Location = "Location",
-    
+
 // }
