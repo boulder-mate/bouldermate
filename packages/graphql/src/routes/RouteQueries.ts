a25d7a7e3836f1d880044ab986fe021cc9f894e3
ethanhusandevup <eth.husband@gmail.com>
2023-08-22 23:00:19 +1000

    Add user route queries with pagination

--- packages/graphql/src/routes/RouteQueries.ts
+++ packages/graphql/src/routes/RouteQueries.ts
@@ -6,25 +6,46 @@ import { Logger } from "../utils/logging";
 
 const logger = new Logger("RouteQueries");
 
+const PAGINATION_SIZE = 50;
+
 export async function getRoutesById(
   obj: any,
   args: any,
   context: AuthContext,
   info: any
 ) {
-  logger.info(`Received route query for IDs ${args.ids}`);
+  logger.info(`Processing route query for IDs ${args.ids}`);
 
   var routes = [];
   for (var id of args.ids) {
     var _id = new ObjectId(args.id);
     var route = (await db.routesCollection?.findOne({ _id })) as Route;
-    logger.info(`Retrieved location ${route.name}`);
+    logger.info(`Retrieved route ${route.name}`);
     routes.push(route);
   }
 
   return routes;
 }
 
+export async function getUserRoutes(
+  obj: any,
+  args: any,
+  context: AuthContext,
+  info: any
+) {
+  var querying = context.user?.routes;
+  if (!querying) return [];
+
+  logger.info(`Received nonempty query for user routes ${querying}`);
+
+  // We use an optional page param for pagination - these return a different slice of the total routes array
+  var page = args.page || 0;
+  querying = querying.slice(page, page + PAGINATION_SIZE);
+
+  return getRoutesById(obj, args, context, info);
+}
+
 export const routeQueries = {
   getRoutesById,
+  getUserRoutes,
 };
