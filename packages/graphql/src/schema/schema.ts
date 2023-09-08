0f440b2f67822818f460b61f931276f009dfadf2
ethanhusandevup <eth.husband@gmail.com>
2023-09-08 16:39:58 +1000

    Rename graphql package

--- packages/graphql/src/schema/schema.ts
+++
@@ -1,9 +0,0 @@
-import { makeExecutableSchema } from "@graphql-tools/schema";
-import { typeDefs } from "./typedefs";
-import { resolvers } from "./resolvers";
-
-// Combine our typedefs + resolvers.
-export const schema = makeExecutableSchema({ typeDefs, resolvers });
-
-
-
