0d2fc3a4e48f0841b79abc7ce06e2e350ddadb7b
ethanhusandevup <eth.husband@gmail.com>
2023-09-13 18:12:54 +1000

    Use Date and no ObjectId in common

--- packages/common/src/abstract.ts
+++ packages/common/src/abstract.ts
@@ -1,16 +1,15 @@
-
 // We can't use ObjectID because it breaks when importing common to the mobile app
 // Expo does not support importing Node source code from a third party file (which is used in mongodb package)
 // However these are always ObjectIDs or strings
 export type ID = {
-    _id: any
-}
+  id: string;
+};
 
-// These strings are those associated with an ObjectID we wish to point to
-export type LinkedID = string
+// These identify in the type definitions that we are pointing to something else's ID
+export type LinkedID = string;
 
 // ISO String for creation of objects
 export type Time = {
-    created: string,
-    last_updated: string
-}
+  created: Date;
+  last_updated: Date;
+};
