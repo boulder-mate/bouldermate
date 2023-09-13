6553e37905b8573900b253b9d5c49ce2320feb65
ethanhusandevup <eth.husband@gmail.com>
2023-09-13 22:27:48 +1000

    Remove common

--- packages/common/src/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./abstract";
-export * from "./feedback";
-export * from "./grades";
-export * from "./locations";
-export * from "./route";
-export * from "./user";
-
-//export * from "./beta";
