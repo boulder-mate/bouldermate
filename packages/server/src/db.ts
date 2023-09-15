b24a8025cf16cb48b5349f1c744ad399e2e20da7
Ethan Husband <ethan.husband@evup.com.au>
2023-09-15 14:57:35 +1000

    Rename app package to mobile-app

--- packages/server/src/db.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-// This layer is included for
-// - If we ever want to vary datasources, we can do so via the PrismaClient props
-// - The app only has one PrismaClient, for the moment
-// - Other operations, including if prisma ever needs to be swapped out
-
-export const db = new PrismaClient();
--- packages/server/src/index.ts
+++
@@ -1,8 +0,0 @@
-import {initApolloServer} from "./server"
-
-// Sequentially initialise all services, then the server itself
-async function startServer() {
-  await initApolloServer();
-}
-
-startServer();
